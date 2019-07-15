d57fd37cb1bbe8852c1f4ada449dd21ebb2160bc
SidneyNiccolson <sidneyniccolson@gmail.com>
2019-07-15 12:18:10 +0200

    remove assets

--- CMLMasterProject/settings.py
+++ CMLMasterProject/settings.py
@@ -40,7 +40,7 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     STATIC_PATH,
-    os.path.join(BASE_DIR, 'assets'),
+    #os.path.join(BASE_DIR, 'assets'),
 
 )
 
