76b875c206c1d248778a367f62f1ad4157302b5a
SidneyNiccolson <sidneyniccolson@gmail.com>
2020-02-26 15:07:35 +0100

    panorama landing page updates

--- panorama/urls.py
+++ panorama/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
         url(r'^services/$', views.services, name='panorama_services'),
         url(r'^about/$', views.about, name='panorama_about'),
         url(r'^contact/$', views.contact, name='panorama_contact'),
+        url(r'^methods/$', views.methods, name='panorama_methods'),
+        url(r'^edu/mfa/$', views.mfa, name='panorama_mfa'),
 ]
--- panorama/views.py
+++ panorama/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,'panorama/home.html')
+    return render(request, 'panorama/home.html')
+
 
 def services(request):
-    return render(request,'panorama/services.html')
+    return render(request, 'panorama/services.html')
+
 
 def contact(request):
-    return render(request,'panorama/contact.html')
+    return render(request, 'panorama/contact.html')
+
 
 def about(request):
-    return render(request,'panorama/about.html')
+    return render(request, 'panorama/about.html')
+
+
+def methods(request):
+    return render(request, 'panorama/methods.html')
+
+
+def mfa(request):
+    return render(request, 'panorama/mfa.html')
